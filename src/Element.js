f80360ef46f721adcd5e9f06a644566adae778a7
Cleidimar Viana <cleidimarviana@gmail.com>
2019-02-20 01:55:39 -0300

    Set Element

--- src/Element.js
+++ src/Element.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import { Col, Popover, Overlay, ButtonToolbar, Tooltip, OverlayTrigger } from 'react-bootstrap';
+import { Col, Popover } from 'react-bootstrap';
 import data from './data.json';
 
 class Element extends Component {
 
   constructor(props, context) {
     super(props, context);
+
+    this.handleClick = ({ target }) => {
+      this.setState(s => ({ target, show: !s.show }));
+    };
+
+    this.state = {
+      show: false,
+    };
   }
 
-  getColor(id) {
+  getColor(id){
     var result = data.categories.find(obj => {
       return obj.id === id
     })
@@ -17,17 +25,18 @@ class Element extends Component {
 
   render() {
 
-    if (this.props.element) {
+    if (this.props.element){
 
-      var item = this.getColor(this.props.element.group);
-      if (item) {
-        return <Col style={{ background: item.colorhexa }}>
-          <span style={{ fontSize: "9px", position: "absolute", marginTop: "2px", marginLeft: "-13px" }} >{this.props.element.atomicnumber}</span>
-          <span>{this.props.element.symbol}</span>
-          
-        </Col>;
-      }
-    } else
+    var item = this.getColor(this.props.element.group);
+    if(item){ 
+      return <Col style={{ background: item.colorhexa }} onClick={this.handleClick}>
+        <span style={{ fontSize: "9px", position: "absolute", marginTop: "2px", marginLeft: "-13px"  }} >{this.props.element.atomicnumber}</span>
+        <span>{this.props.element.symbol}</span>
+      
+      </Col>
+      ;
+    }
+    }else
       return <Col className="col2"></Col>;
   }
 }
